0d20dfd68b5dffb29e4b502c8dd46f7f1954d979
lcpereztorres96 <77509432+galvushow@users.noreply.github.com>
2023-07-16 19:46:34 -0500

    update reducer

--- frontend/src/components/Application.tsx
+++ frontend/src/components/Application.tsx
@@ -35,7 +35,8 @@ export default function Application() {
                             <Route path="/sign-up" element={<SignUp onError={setError} to="/" />} />
                             <Route path="/sign-in" element={<SignIn onError={setError} to="/" />} />
                             <Route path="/project" element={<Main><ProjectTable /></Main>} />
-                            <Route path="/project/:id" element={<Main>¡Hola, Mundo!</Main>} />
+                            <Route path="/project/:id" element={<Main >¡Hola, Mundo!</Main>} />
+                            <Route path="/project/:id/board" element={<Main >¡Hola, Mundo!</Main>} />
                             <Route path="/project-form" element={<DialogForm open={true} />} />
                         </Routes>
                     </BrowserRouter>
