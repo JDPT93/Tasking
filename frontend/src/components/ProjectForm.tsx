c1088b2df1d3f971cb1ab74598d4496ad90d1eed
JDPT93 <josedanielpereztorres@gmail.com>
2023-07-20 12:47:36 -0500

    reset project form

--- frontend/src/components/ProjectForm.tsx
+++ frontend/src/components/ProjectForm.tsx
@@ -15,12 +15,10 @@ export default function ProjectForm({ onError, onSuccess }: Properties) {
     const locale = React.useContext(LocaleContext);
     const { projectService } = React.useContext(ProjectService);
     const { user } = React.useContext(UserContext);
-    const [name, setName] = React.useState<String | null>('');
-    const [description, setDescription] = React.useState<String | null>('');
     return (
         <Stack padding="4px 0px" width={450} alignItems="center" component="form" gap={2} onSubmit={event => {
             event.preventDefault();
-            const { nameField, descriptionField } = event.target as HTMLFormElement;
+            const { nameField, descriptionField, reset } = event.target as HTMLFormElement;
             projectService.create({
                 leader: user!,
                 name: nameField.value,
@@ -32,16 +30,15 @@ export default function ProjectForm({ onError, onSuccess }: Properties) {
                     if (!response.ok) {
                         throw body as Error;
                     }
-                    setName('');
-                    setDescription('');
                     if (onSuccess !== undefined) {
                         onSuccess(body as Project)
                     }
+                    reset();
                 })
                 .catch(onError);
         }}>
-            <TextField onChange={(event) => setName(event.target.value) } value={name} fullWidth label={locale.schemas.project.properties.name} name="nameField" required type="text" variant="outlined" />
-            <TextField onChange={(event) => setDescription(event.target.value) } value={description} fullWidth label={locale.schemas.project.properties.description} name="descriptionField" required type="text" variant="outlined" multiline rows={4} />
+            <TextField fullWidth label={locale.schemas.project.properties.name} name="nameField" required type="text" variant="outlined" />
+            <TextField fullWidth label={locale.schemas.project.properties.description} name="descriptionField" required type="text" variant="outlined" multiline rows={4} />
             <Button fullWidth type="submit" variant="contained">{locale.actions.btnSaveProject}</Button>
         </Stack>
     );
