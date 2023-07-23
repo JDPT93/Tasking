344800ac878e59e397466d2b8d5455c90f6c5c04
lcpereztorres96 <77509432+galvushow@users.noreply.github.com>
2023-07-23 17:38:41 -0500

    update

--- frontend/src/components/IssueCard.tsx
+++ frontend/src/components/IssueCard.tsx
@@ -10,28 +10,6 @@ interface Properties {
     issue: Issue;
 }
 
-const stringToColor = (string: string) => {
-    let hash = 0, i;
-    for (i = 0; i < string.length; i += 1) {
-        hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    let color = '#';
-    for (i = 0; i < 3; i += 1) {
-        const value = (hash >> (i * 8)) & 0xff;
-        color += `00${value.toString(16)}`.slice(-2);
-    }
-    return color;
-}
-
-const stringAvatar = (name: string) => {
-    return {
-        sx: {
-            bgcolor: stringToColor(name),
-        },
-        children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
-    };
-}
-
 export function IssueCard({ issue }: Properties) {
     const locale = React.useContext(LocaleContext);
     return (<CardActionArea onAuxClick={event => {
